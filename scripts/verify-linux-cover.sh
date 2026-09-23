2e2f6172bd4301917da3c59b3f5c61e733a4c14a
Analyzer <analyzer@samson.tv>
2026-09-23 20:24:53 +0800

    build: stamp Linux verification binary with current commit

--- scripts/verify-linux-cover.sh
+++ scripts/verify-linux-cover.sh
@@ -16,7 +16,7 @@ go test ./...
 go vet ./...
 cd "$project_root"
 mkdir -p dist
-commit=${TDL_BUILD_COMMIT:-unknown}
+commit=${TDL_BUILD_COMMIT:-$(git rev-parse --short=12 HEAD)}
 build_date=$(date -u +%Y-%m-%dT%H:%M:%SZ)
 CGO_ENABLED=0 go build -trimpath -ldflags "-X github.com/iyear/tdl/pkg/consts.Commit=$commit -X github.com/iyear/tdl/pkg/consts.CommitDate=$build_date" -o dist/tdl-linux-amd64 .
 ./dist/tdl-linux-amd64 version
